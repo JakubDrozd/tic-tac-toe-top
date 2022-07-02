68ae12e71e7dfeefac83e207f0ffb1654f538a21
Veanty <plmulticraftpl@gmail.com>
2022-07-02 18:51:02 +0200

    Feat winning conditions and winning console log

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
 
 const cellElements = document.querySelectorAll("[data-cell");
 
@@ -13,7 +27,19 @@ function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   swapTurns();
+}
+
+function endGame(draw) {
+  if (draw) {
+  } else {
+    winningMessageTextElement.innerText = `${
+      circleTurn ? "O's" : "X's"
+    } Wins! `;
+  }
 }
 
 function placeMark(cell, currentClass) {
@@ -22,4 +48,12 @@ function placeMark(cell, currentClass) {
 
 function swapTurns() {
   circleTurn = !circleTurn;
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
