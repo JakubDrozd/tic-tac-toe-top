5819f7774c337022392095a764a5eacb4e45343f
Veanty <plmulticraftpl@gmail.com>
2022-07-02 15:26:56 +0200

    Feat createPlayer factory function to create players

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const playerFactory = (name) => {
-  let score = 0;
-  const getScore = () => {
-    console.log(score);
-  };
-  return { name, getScore };
+const createPlayer = (name) => {
+  let _score = 0;
+  (getScore = () => {
+    console.log(_score);
+  }),
+    (setScore = (newScore) => {
+      _score = newScore;
+    });
+  return { name, getScore, setScore };
 };
 
-const p1 = playerFactory("jakub");
+const p1 = createPlayer("Jakub");
+
+const p2 = createPlayer("Piotr");
